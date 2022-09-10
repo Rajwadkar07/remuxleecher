d9859c8e3d2ace270afe1bbec6f489c8e2ea41f6
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2022-09-10 21:37:44 +0530

    Fixed new and old lists.

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import feedparser
 from time import sleep
 from pyrogram import Client
+
 app = Client("my_account")
-filter = ["remux", "1080p"]
+key = ["remux", "1080p"]
+print("Restarting")
 old = []
 while True:
-  res = feedparser.parse("https://rss24h.torrentleech.org/98d5a4ce118834c42aad")
-  new = []
-  with open('links.txt', 'w+') as f:
+    res = feedparser.parse("https://rss24h.torrentleech.org/98d5a4ce118834c42aad")
+    new = []
     for i in res.entries:
-      if all(x in i.title.lower() for x in filter):
-        command = "/mirror "+i.link
-        if command not in old:
-          new += [command]
-          async def main():
-            async with app:
-              await app.send_message(-1001618019889, command)
-          f.write(command+"\n")
-          app.run(main())
-          print("Sent "+i.title)
-          sleep(60)
-  old = new
-  print("Sleeping")
-  sleep(10)
+        if all(x in i.title.lower() for x in key):
+            command = "/mirror " + str(i.link)
+            new += [command]
+    for i in new:
+        if i not in old:
+            async def main():
+                async with app:
+                    await app.send_message(-1001618019889, i)
+            app.run(main())
+            print("Sent " + str(i)[8:])
+            sleep(10)
+    old = new
+    print("Length of new: " + str(len(new)))
+    print("Length of old: " + str(len(old)))
+    print("Sleeping for 5 minutes.")
+    sleep(300)
