724ac3c36155e8abf0c148a645950a0288c77e71
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2022-09-10 16:31:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import feedparser
+from time import sleep
+from pyrogram import Client
+app = Client("my_account")
+filter = ["remux", "1080p"]
+old = []
+while True:
+  res = feedparser.parse("https://rss24h.torrentleech.org/98d5a4ce118834c42aad")
+  new = []
+  with open('links.txt', 'w+') as f:
+    for i in res.entries:
+      if all(x in i.title.lower() for x in filter):
+        command = "/mirror "+i.link
+        if command not in old:
+          new += [command]
+          async def main():
+            async with app:
+              await app.send_message("me", command)
+          f.write(command+"\n")
+          app.run(main())
+          print("Sent "+i.title)
+          sleep(60)
+  old = new
+  print("Sleeping")
+  sleep(10)
